d7b5a55f703b426ce787fda1e84b6fdeeb8a2671
mohammadjelodar13657 <mohammad.jelodar@gmail.com>
2023-05-20 22:09:06 +0430

    Add Todo Fragment

---
+++ app/src/main/java/com/example/taskmanagerapp/ui/task_manager/todo_fragment/TodoFragment.kt
@@ -0,0 +1,27 @@
+package com.example.taskmanagerapp.ui.task_manager.todo_fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.taskmanagerapp.R
+import com.example.taskmanagerapp.databinding.FragmentTodoBinding
+
+class TodoFragment : Fragment() {
+
+    private lateinit var dataBinding: FragmentTodoBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_todo, container, false)
+        return inflater.inflate(R.layout.fragment_todo, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
