cf57941e0db2d04ada96d3a7c00db0ed93f88c49
mohammadjelodar13657 <mohammad.jelodar@gmail.com>
2023-05-20 22:06:48 +0430

    Add Main Fragment

---
+++ app/src/main/java/com/example/taskmanagerapp/ui/task_manager/main_fragment/MainFragment.kt
@@ -0,0 +1,27 @@
+package com.example.taskmanagerapp.ui.task_manager.main_fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.taskmanagerapp.R
+import com.example.taskmanagerapp.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private lateinit var dataBinding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
+        return dataBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
