7e3dd69d78ebe9f7444a87f6b41719f7ae340e67
mohammadjelodar13657 <mohammad.jelodar@gmail.com>
2023-05-20 22:03:20 +0430

    Add Register Fragment

---
+++ app/src/main/java/com/example/taskmanagerapp/ui/login_register/register_frament/RegisterFragment.kt
@@ -0,0 +1,29 @@
+package com.example.taskmanagerapp.ui.login_register.register_frament
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.taskmanagerapp.R
+import com.example.taskmanagerapp.databinding.FragmentRegisterBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class RegisterFragment : Fragment() {
+
+    private lateinit var dataBinding: FragmentRegisterBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_register, container, false)
+        return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
