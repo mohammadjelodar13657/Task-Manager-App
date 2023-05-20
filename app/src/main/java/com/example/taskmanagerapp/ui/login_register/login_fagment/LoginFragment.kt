daafd0ad45488f305be0579b21fad53b8c8212f3
mohammadjelodar13657 <mohammad.jelodar@gmail.com>
2023-05-20 21:49:26 +0430

    Add Login Fragment

---
+++ app/src/main/java/com/example/taskmanagerapp/ui/login_register/login_fagment/LoginFragment.kt
@@ -0,0 +1,27 @@
+package com.example.taskmanagerapp.ui.login_register.login_fagment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.taskmanagerapp.R
+import com.example.taskmanagerapp.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private lateinit var dataBinding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        return dataBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
