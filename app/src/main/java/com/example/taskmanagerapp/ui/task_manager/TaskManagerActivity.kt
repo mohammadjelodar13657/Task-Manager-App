c1bd3ebc2a9a95883d7137d7264a4fcc419f462f
mohammadjelodar13657 <mohammad.jelodar@gmail.com>
2023-05-20 21:36:52 +0430

    Add Task Manager Activity

---
+++ app/src/main/java/com/example/taskmanagerapp/ui/task_manager/TaskManagerActivity.kt
@@ -0,0 +1,12 @@
+package com.example.taskmanagerapp.ui.task_manager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.taskmanagerapp.R
+
+class TaskManagerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task_manager)
+    }
+}
