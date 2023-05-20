dffad873a9f5a6f014cbaecbd281db308ca40454
mohammadjelodar13657 <mohammad.jelodar@gmail.com>
2023-05-20 22:19:32 +0430

    Add Done Fragment

---
+++ app/src/main/java/com/example/taskmanagerapp/ui/task_manager/done_fragment/DoneFragment.kt
@@ -0,0 +1,28 @@
+package com.example.taskmanagerapp.ui.task_manager.done_fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.taskmanagerapp.R
+import com.example.taskmanagerapp.databinding.FragmentDoneBinding
+
+class DoneFragment : Fragment() {
+
+    private lateinit var dataBinding: FragmentDoneBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_done, container, false)
+        return dataBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
