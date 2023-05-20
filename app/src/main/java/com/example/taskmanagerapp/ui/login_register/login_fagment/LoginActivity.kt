9e22974d0891002b2a50e52fa88e7b53e42c813b
mohammadjelodar13657 <mohammad.jelodar@gmail.com>
2023-05-20 21:33:22 +0430

    Add Login Activity

---
+++ app/src/main/java/com/example/taskmanagerapp/ui/login_register/login_fagment/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.taskmanagerapp.ui.login_register.login_fagment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.taskmanagerapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
